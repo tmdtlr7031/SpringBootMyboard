6434f84c6a5219f89dcb7d49cc92ff5a627cc463
namseungsik <tmd7031@naver.com>
2021-08-23 23:56:19 +0900

    #7 global cache 설정
    - no-store 캐시 정책 이용
    - CacheControl에서 no-store, no-cache 사용 불가, 해결방안 필요(대응방안 못 찾음)

---
+++ src/main/java/nss/myboard/global/config/WebConfig.java
@@ -0,0 +1,28 @@
+package nss.myboard.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    /**
+     * Cache-Control 설정 방법들 (https://www.baeldung.com/spring-mvc-cache-headers)
+     * 참고
+     *  : WebContentInterceptor를 이용한 방법도 HTTP 1.0 헤더 미지원 추세인듯 하다..(https://prettymucho.tistory.com/4 옵션 설명 참고)
+     *  : 캐시 무효화 : https://www.lesstif.com/software-architect/http-https-browser-caching-http-header-20775788.html
+     */
+
+    // 해당 방법은 Cache-Control: no-Store, no-Cache, must-Revalidate 이렇게 다 적용이 안된다. --> 각 요청마다는 response를 쓰면 되지만 전역으로 설정하는 건 모르겠다..
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("**/*")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.noStore()) // 캐시 저장 X
+        ;
+
+    }
+}
